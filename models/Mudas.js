8754ebd16f5123ad8d74428c9cbed97751296415
Edimichael Lima <edimichael.lima@gmail.com>
2022-11-25 18:57:06 -0300

    Add files

---
+++ models/Mudas.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const Mudas = mongoose.model('Mudas', {
+   id: String,
+   usuario: String,
+   subprefeitura: String,
+   arvore: String,
+   quantidade: Number,
+   data: Date,
+   cep: String,  
+})
+
+module.exports = Mudas
