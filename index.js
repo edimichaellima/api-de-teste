6d4a450e46af3419106b9581814b8ed4690454a3
Edimichael Lima <edimichael.lima@gmail.com>
2022-11-26 16:24:37 -0300

    add

--- index.js
+++ index.js
@@ -1,49 +1,12 @@
-// configuração inicial
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express")
 const app = express()
 
+const port = process.env.PORT || 3000
 
-// forma de ler json / middleware
-app.use(
-    express.urlencoded({
-        extended: true,
-    })
-)
-
-app.use(express.json())
-
-//rotas da API
-const rotasMudas = require('./routes/rotasMudas')
-
-app.use('/mudas', rotasMudas)
-
-// rota inicial // endpoint
-app.get('/', (req, res) => {
-
-    // mostrar requisição
-    res.json({ massage: 'Mostrar requisições' })
-
+app.get("/", function (req, res) {
+  res.send("Olá")
 })
 
-//entregar uma porta
-
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = encodeURIComponent (process.env.DB_PASSWORD)
-
-mongoose
-    .connect(
-        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apiclustermaisarvore.7k4spzi.mongodb.net/bancoapima?retryWrites=true&w=majority`
-)
-.then(() => {
-    console.log('Conectado ao MongoDB!')
+app.listen(port, () => {
+  console.info("Rodando...")
 })
-
-.catch((err) => console.log(err))
-
-
-const PORT = process.env.PORT || 8089
-    app.listen(PORT,() => {
-      console.log("Servidor rodando!");
-    })
